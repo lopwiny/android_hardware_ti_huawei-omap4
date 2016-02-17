d564c7ad61f1569342dadd200c0211ec57284da1
ShevT <shev_t@mail.ru>
2016-02-17 11:48:02 +0300

    front: Fixed preview freezes for back camera on zooming

--- camera/CameraHal.cpp
+++ camera/CameraHal.cpp
@@ -599,7 +599,7 @@ int CameraHal::setParameters(const android::CameraParameters& params)
             if (isParameterValid(valstr, mCameraProperties->get(CameraProperties::SUPPORTED_FOCUS_MODES))) {
                 CAMHAL_LOGDB("Focus mode set %s", valstr);
 
-#ifdef CAMERAHAL_TUNA
+#ifdef CAMERAHAL_TUNA || CAMERAHAL_HUAWEI_OMAP4
                 // FOCUS_MODE_CONTINUOUS_PICTURE and FOCUS_MODE_CONTINUOUS_VIDEO are the same in Tuna Ducati
                 // implementation (both point to OMX_IMAGE_FocusControlAuto), so setting / resetting the video
                 // mode based on this distinction achieves nothing but constant preview restarts. Don't bother
